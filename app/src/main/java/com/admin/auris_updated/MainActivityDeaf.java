8d276ac18c6f64bcaca2f9ab1d8e0c61314efe93
Tomson <tomsonthomas68@gmail.com>
2019-05-09 13:35:07 +0530

    3rd commit

--- app/src/main/java/com/admin/auris_updated/MainActivityDeaf.java
+++ app/src/main/java/com/admin/auris_updated/MainActivityDeaf.java
@@ -1,11 +1,15 @@
 package com.admin.auris_updated;
 
 import android.app.Dialog;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
@@ -29,7 +33,12 @@ import com.admin.auris_updated.Services.AlertService;
 import com.admin.auris_updated.Services.EmergencyService;
 import com.admin.auris_updated.Services.EventService;
 
+import java.io.File;
 import java.util.ArrayList;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 
 public class MainActivityDeaf extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -124,13 +133,13 @@ public class MainActivityDeaf extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.caption) {
-            Intent intent = new Intent();
+            /*Intent intent = new Intent();
             intent.setType("video/*");
             intent.setAction(Intent.ACTION_GET_CONTENT);
-            startActivityForResult(Intent.createChooser(intent,"Select Video"),1);
-           // Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Video.Media.INTERNAL_CONTENT_URI);
-           // i.setType("video/*");
-          //  startActivityForResult(i, SELECT_VIDEO);
+            startActivityForResult(Intent.createChooser(intent,"Select Video"),1);*/
+           Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Video.Media.INTERNAL_CONTENT_URI);
+            i.setType("video/*");
+            startActivityForResult(i, SELECT_VIDEO);
         } else if (id == R.id.talk) {
             Intent intent = new Intent(getApplicationContext(), TexttoSpeech.class);
             startActivity(intent);
@@ -175,6 +184,7 @@ public class MainActivityDeaf extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -182,7 +192,7 @@ public class MainActivityDeaf extends AppCompatActivity
             if (requestCode == 1) {
                 Uri selectedImageUri = data.getData();
                 // MEDIA GALLERY
-                String  selectedImagePath = getPath(selectedImageUri);
+                String  selectedImagePath = getPath(MainActivityDeaf.this,selectedImageUri);
                 System.out.print(selectedImagePath);
                 // File source = new File(selectedImagePath);
                 // String target = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)+"/target.mp3";
@@ -196,7 +206,7 @@ public class MainActivityDeaf extends AppCompatActivity
 
     //UPDATED!
     public String getPath(Uri uri) {
-        String[] projection = { MediaStore.Video.Media.DATA };
+        String[] projection = { MediaStore.Video.Media.DATA};
         Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
         if (cursor != null) {
             // HERE YOU WILL GET A NULLPOINTER IF CURSOR IS NULL
@@ -236,4 +246,137 @@ public class MainActivityDeaf extends AppCompatActivity
         }
 
     }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public static String getPath( Context context, Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+
+                // TODO handle non-primary volumes
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{
+                        split[1]
+                };
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the value of the data column for this Uri. This is useful for
+     * MediaStore Uris, and other file-based ContentProviders.
+     *
+     * @param context       The context.
+     * @param uri           The Uri to query.
+     * @param selection     (Optional) Filter used in the query.
+     * @param selectionArgs (Optional) Selection arguments used in the query.
+     * @return The value of the _data column, which is typically a file path.
+     */
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    public static MultipartBody.Part getMultiPartBody(String key, String mMediaUrl) {
+        if (mMediaUrl != null) {
+            File file = new File(mMediaUrl);
+            RequestBody requestFile = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+            return MultipartBody.Part.createFormData(key, file.getName(), requestFile);
+        } else {
+            return MultipartBody.Part.createFormData(key, "");
+        }
+    }
+
 }
